413fbbfb5968504b39bb066bad01e2ee5dcb4a38
Bianca-PT <bianca.kisten@playful.today>
2026-07-28 15:27:35 +0200

    tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-SZVTS0YC58"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-SZVTS0YC58');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
         <Analytics />
